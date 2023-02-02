db213c60efcee3241d7e76c908521bd9627616a1
Ashish Saurav <ashishsaurav2410@gmail.com>
2023-02-03 01:05:23 +0530

    added signin feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import HomeScreen from './Components/HomeScreen.js'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginScreen from './Components/LoginScreen';
+import { auth } from './firebase';
 
 function App() {
   const user = null;
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        // Logged in
+        console.log(userAuth)
+      } else {
+        //Logged out
+      }
+    })
+    return unsubscribe;
+  }, [])
+
   return (
     <div className="app">
       <BrowserRouter>
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
     apiKey: "AIzaSyDN_Rh-QHSVlO6PNLnwiHQkeppZyGEsknI",
     authDomain: "netflix-clone-bed6b.firebaseapp.com",
@@ -6,3 +10,10 @@ const firebaseConfig = {
     messagingSenderId: "758233233593",
     appId: "1:758233233593:web:30a4d6843d59c74f624368"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth }
+export default db;
