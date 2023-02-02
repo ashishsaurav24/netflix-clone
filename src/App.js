4c62c070f1f3a2cb121a181467004157a39e9feb
Ashish Saurav <ashishsaurav2410@gmail.com>
2023-02-03 02:05:57 +0530

    added redux functionality

--- src/App.js
+++ src/App.js
@@ -4,17 +4,24 @@ import HomeScreen from './Components/HomeScreen.js'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginScreen from './Components/LoginScreen';
 import { auth } from './firebase';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch()
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(userAuth => {
       if (userAuth) {
         // Logged in
-        console.log(userAuth)
+        dispatch(login({
+          uid: userAuth.uid,
+          email: userAuth.email
+        }))
       } else {
         //Logged out
+        dispatch(logout)
       }
     })
     return unsubscribe;
---
+++ src/Components/ProfileScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './ProfileScreen.css'
+
+function ProfileScreen() {
+  return (
+    <div className='profileScreen'>
+        
+
+    </div>
+  )
+}
+
+export default ProfileScreen
